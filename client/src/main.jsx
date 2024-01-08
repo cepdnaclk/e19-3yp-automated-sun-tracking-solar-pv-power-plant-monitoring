90b600d7549fff46f56a40f1b9b737dbafe51782
Kavishkagaya <kavishkarambukwella@gmail.com>
2024-01-08 14:20:37 +0530

    added default fallback method when unauthorized

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import axios from 'axios';
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = "http://localhost:5000/api";
+// set default fallback methode when unauthorized
+axios.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response.status === 401) {
+      // send refresh token to server
+      const refreshToken = localStorage.getItem("refreshToken");
+      axios
+        .post("/api/login/token", { refreshToken })
+        .then((res) => {
+          // set token, refresh token and username in local storage
+          localStorage.setItem("token", res.data.accessToken);
+          localStorage.setItem("refreshToken", res.data.refreshToken);
+          localStorage.setItem("username", res.data.username);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+          // set token in axios header
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + res.data.accessToken;
+
+          // retry request
+          return axios(err.config);
+        })
+        .catch((err) => {
+          // redirect to login page
+          window.location.href = "/login?error=Login%20expired";
+        });
+    }
+    return Promise.reject(err);
+  }
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
-	</React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
