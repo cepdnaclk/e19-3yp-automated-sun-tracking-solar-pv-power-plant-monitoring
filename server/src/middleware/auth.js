6b6c2642d58fbc5f72cb0bd90770377709e8c76e
Dinuka <e19029@eng.pdn.ac.lk>
2024-01-08 19:37:39 +0530

    devices and companies API changes

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -10,11 +10,12 @@ function authenticateToken(req, res, next) {
     if (token == null)
       return res.sendStatus(401).json({ error: "Unauthorized" });
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, username, user_type, id) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, username, user_type, id, email) => {
       if (err) throw err;
       req.username = username;
       req.user_type = user_type;
       req.user_id = id
+      req.email =email
       next();
     });
   } catch (err) {
@@ -22,15 +23,15 @@ function authenticateToken(req, res, next) {
   }
 }
 
-function generateAccessToken(username,user_type,id) {
-  return jwt.sign({ username: username, user_type:user_type, id:id }, process.env.ACCESS_TOKEN_SECRET, {
+function generateAccessToken(username,user_type,id,email) {
+  return jwt.sign({ username: username, user_type:user_type, id:id, email:email }, process.env.ACCESS_TOKEN_SECRET, {
     // Expire token in 20 mins
     expiresIn: "1200s",
   });
 }
 
-function generateRefreshToken(username,user_type,id) {
-  return jwt.sign({ username: username, user_type:user_type, id:id }, process.env.REFRESH_TOKEN_SECRET);
+function generateRefreshToken(username,user_type,id,email) {
+  return jwt.sign({ username: username, user_type:user_type, id:id , email:email}, process.env.REFRESH_TOKEN_SECRET);
 }
 
 module.exports = {
--- server/src/routes/devices.js
+++ server/src/routes/devices.js
@@ -9,16 +9,22 @@ const {
   objectKeysSnakeToCamel,
 } = require("../utils/parse");
 
+const { authenticateToken } = require("../middleware/auth");
+
 // to execute and get the output of the queries easily
 const { execQuery } = require("../database/database");
 
-// View devices - for the admins
-// 
-//change req.query to req.body if necessary
+if(req.user_type == "company"){
+} else {
+  return res.sendStatus(401).json({ error: "Unauthorized" });
+}
 
-router.get("/", (req, res, next) => {
+
+// View devices - for the admins - [Done]
+router.get("/", authenticateToken, (req, res, next) => {
+  if(req.user_type == "admin"){
     if (req.query.id) {
-      execQuery(`SELECT id, company_name, email, company_address, contact_no1, contact_no2 FROM company WHERE id=${req.query.id}`)
+      execQuery(`SELECT id, model_name, model_number, assigned_company_id FROM device WHERE id=${req.query.id}`)
         .then((rows) => {
           data = objectKeysSnakeToCamel(rows[0]);
           res.status(200).json(data);
@@ -27,7 +33,7 @@ router.get("/", (req, res, next) => {
           next(err);
         });
     } else {
-      execQuery(`SELECT id, company_name, email, company_address, contact_no1, contact_no2 FROM company`)
+      execQuery(`SELECT id, model_name, model_number, assigned_company_id FROM device`)
         .then((rows) => {
           data = rows[0].map((row) => objectKeysSnakeToCamel(row));
           res.status(200).json(data);
@@ -36,10 +42,90 @@ router.get("/", (req, res, next) => {
           next(err);
         });
     }
+  } else {
+    return res.sendStatus(401).json({ error: "Unauthorized" });
+  }
   });
 
-  //view devices - for companies
-  //view devices - for customers
+  //view devices - for companies - [Done]
+  // request format { id: device_id}
+  router.get("/", authenticateToken, (req, res, next) => {
+    if(req.user_type == "company"){
+      if (req.query.id) {
+        execQuery(`SELECT id, model_name, model_number, description_, purchased_customer_email FROM device WHERE id=${req.query.id} AND assigned_company_id=${req.user_id}`)
+          .then((rows) => {
+            data = objectKeysSnakeToCamel(rows[0]);
+            res.status(200).json(data);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } else {
+        execQuery(`SELECT id, model_name, model_number, description_, purchased_customer_email FROM device WHERE assigned_company_id=${req.user_id}`)
+          .then((rows) => {
+            data = rows[0].map((row) => objectKeysSnakeToCamel(row));
+            res.status(200).json(data);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      }
+    } else {
+      return res.sendStatus(401).json({ error: "Unauthorized" });
+    }
+    });
+
+  //view devices - from customer's app
+  router.get("/", authenticateToken, (req, res, next) => {
+    if(req.user_type == "customer"){
+      if (req.query.id) {
+        execQuery(`SELECT device_name_by_customer, model_name, model_number, description_ 
+                    FROM device WHERE assigned_customer_id=${req.user_id} AND id=${req.query.id}`)
+          .then((rows) => {
+            data = objectKeysSnakeToCamel(rows[0]);
+            res.status(200).json(data);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } else {
+        execQuery(`SELECT device_name_by_customer, model_name, model_number, description_ 
+        FROM device WHERE assigned_customer_id=${req.user_id}`)
+          .then((rows) => {
+            data = rows[0].map((row) => objectKeysSnakeToCamel(row));
+            res.status(200).json(data);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      }
+    } else {
+      return res.sendStatus(401).json({ error: "Unauthorized" });
+    }
+    });
+
+  // view contact details of the device's company - Contact support
+  // request format { id: device_id}
+  router.get("/", authenticateToken, (req, res, next) => {
+    if(req.user_type == "customer"){
+      if (req.query.id) {
+        execQuery(`SELECT username, email, contact_number, user_address 
+                    FROM user WHERE id=
+                    (SELECT assigned_company_id
+                      FROM device WHERE id= ${req.query.id})`)	
+          .then((rows) => {
+            data = objectKeysSnakeToCamel(rows[0]);
+            res.status(200).json(data);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } 
+    } else {
+      return res.sendStatus(401).json({ error: "Unauthorized" });
+    }
+    });
+
   
   //add new device - only accessible for admins - [Done]
   //
@@ -50,21 +136,26 @@ router.get("/", (req, res, next) => {
   // "assigned_company_id" : xxxx
   //}
   router.post("/", (req, res, next) => {
-    try {
-      // const [fields, values] = requestBodyToFieldsAndValues(req.body);
-      const [fields, values] = [Object.keys(req.body), Object.values(req.body)];
-      const createdeviceQuery = `INSERT INTO device (${fields.toString()}) VALUES (${values.toString()})`;
-  
-      execQuery(createdeviceQuery)
-        .then((rows) => {
-          res.status(200).json({ message: "New Device created successfully" });
-        })
-        .catch((err) => {
-          next(err);
-        });
-    } catch (err) {
-      next(err);
+    if(req.user_type == "admin"){
+      try {
+        // const [fields, values] = requestBodyToFieldsAndValues(req.body);
+        const [fields, values] = [Object.keys(req.body), Object.values(req.body)];
+        const createdeviceQuery = `INSERT INTO device (${fields.toString()}) VALUES (${values.toString()})`;
+    
+        execQuery(createdeviceQuery)
+          .then((rows) => {
+            res.status(200).json({ message: "New Device created successfully" });
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      return res.sendStatus(401).json({ error: "Unauthorized" });
     }
+    
   });
 
   
@@ -72,15 +163,52 @@ router.get("/", (req, res, next) => {
   
   // update the device details by the company - only from admin
   // {
-  //   "model_name": "Example Company",
-  // "model_number": "example@example.com",
+  //   "model_name": "Example Model",
+  // "model_number": "efwfr",
   // "assigned_company_id" : xxxx   
   // }
-  
-  // adding necessary details by the company request format - only from company
+
+  router.put("/", (req, res, next) => {
+    if(req.user_type == "admin"){
+      try {
+        const id = req.body["id"];
+        delete req.body["id"]; //id used in the UPDATE query, not needed in the update string
+        const [fields, values] = [Object.keys(req.body), Object.values(req.body)];
+        // Combine the two arrays into a single array.
+        let updateString = "";
+    
+        for (let i = 0; i < fields.length; i++) {
+          updateString += fields[i] + " = ";
+          updateString += `'${values[i]}', `;
+        }
+    
+        // remove last trailling ", "
+        updateString = updateString.substring(0, updateString.length - 2);
+    
+        const updateDeviceDataAdminQuery = `UPDATE device SET ${updateString} WHERE id='${id}';`;
+    
+        execQuery(updateDeviceDataAdminQuery)
+          .then((rows) => {
+            res
+              .status(200)
+              .json({ message: "Device Specifications updated successfully" });
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      return res.sendStatus(401).json({ error: "Unauthorized" });
+    }
+  }); 
+
+  // adding necessary details by the company - only from company 
+  // (edit device by company)
+  // request format 
   // {
   //   id: 123456, //device id
-  //   dimension: "11.2 x 34.5",
   //   description: "123 Descrption",
   //   purchased_customer_email: "purchasedcustomer@gmail.com"  //company should add the customer's email when they are selling the device
   // }
@@ -88,80 +216,78 @@ router.get("/", (req, res, next) => {
   // This API updates anything that is given by the request format
   
   router.put("/", (req, res, next) => {
-    try {
-      const id = req.body["id"];
-      delete req.body["id"]; //id used in the UPDATE query, not needed in the update string
-      const [fields, values] = [Object.keys(req.body), Object.values(req.body)];
-      // Combine the two arrays into a single array.
-      let updateString = "";
-  
-      for (let i = 0; i < fields.length; i++) {
-        updateString += fields[i] + " = ";
-        updateString += `'${values[i]}', `;
+    if(req.user_type == "company"){
+      try {
+        const id = req.body["id"];
+        delete req.body["id"]; //id used in the UPDATE query, not needed in the update string
+        const [fields, values] = [Object.keys(req.body), Object.values(req.body)];
+        // Combine the two arrays into a single array.
+        let updateString = "";
+    
+        for (let i = 0; i < fields.length; i++) {
+          updateString += fields[i] + " = ";
+          updateString += `'${values[i]}', `;
+        }
+    
+        // remove last trailling ", "
+        updateString = updateString.substring(0, updateString.length - 2);
+    
+        const updateDeviceDataCompanyQuery = `UPDATE device SET ${updateString} WHERE id='${id}';`;
+    
+        execQuery(updateDeviceDataCompanyQuery)
+          .then((rows) => {
+            res
+              .status(200)
+              .json({ message: "Device Specifications updated successfully" });
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } catch (err) {
+        next(err);
       }
-  
-      // remove last trailling ", "
-      updateString = updateString.substring(0, updateString.length - 2);
-  
-      const updateDeviceDataQuery = `UPDATE device SET ${updateString} WHERE id='${id}';`;
-  
-      execQuery(updateDeviceDataQuery)
-        .then((rows) => {
-          res
-            .status(200)
-            .json({ message: "Device Specifications updated successfully" });
-        })
-        .catch((err) => {
-          next(err);
-        });
-    } catch (err) {
-      next(err);
+    } else {
+      return res.sendStatus(401).json({ error: "Unauthorized" });
     }
   });
 
-  // assigning a customer to the device - only from customer
+  // Add device by customer to their app - [Done]
   //  { 
-  //   "id": 123456, //device id
-  //   "email": "useremail@gmail.com" //customer email
-  //   "assigned_customer_id":654321  
-  //    device_location:"79.256598 10.125689"
+  //   id: 123456, //device id
+  //    device_name_by_customer: "Roof 1 Home 1",
+  //    device_latitude:"79.256598"
+  //    device_longitude:"10.125689"
   // }
 
-  router.put("/", (req, res, next) => {
-    try {
-      // const id = req.body["id"];
-      // const email = req.body["email"];
-      // delete req.body["id"];
-      // delete req.body["email"]; //id and email used in the UPDATE query, not needed in the update string
-      // const [fields, values] = [Object.keys(req.body), Object.values(req.body)];
-      // // Combine the two arrays into a single array.
-      // let updateString = "";
-  
-      // for (let i = 0; i < fields.length; i++) {
-      //   updateString += fields[i] + " = ";
-      //   updateString += `'${values[i]}', `;
-      // }
-  
-      // // remove last trailling ", "
-      // updateString = updateString.substring(0, updateString.length - 2);
-  
-      // set the devices's assigned customer id to user's id, 
-      // used the purchased customer email to check whether the correct user is adding the device
-      const assignCustomertoDeviceQuery = `UPDATE device SET assigned_customer_id=${req.body.assigned_customer_id} WHERE id=${req.body.id} AND
-      (SELECT purchased_customer_email FROM device WHERE id=${req.body.id}) = ${req.body.email};`;
-  
-      execQuery(assignCustomertoDeviceQuery)
-        .then((rows) => {
-          res
-            .status(200)
-            .json({ message: "Device Specifications updated successfully" });
-        })
-        .catch((err) => {
-          next(err);
-        });
-    } catch (err) {
-      next(err);
+  router.put("/", authenticateToken, (req, res, next) => {
+    if(req.user_type == "customer"){
+      try {
+        // set the devices's assigned customer id to user's id, 
+        // used the purchased customer email to check whether the correct user is adding the device
+        const assignCustomertoDeviceQuery = `UPDATE device SET 
+                                              assigned_customer_id=${req.user_id}, 
+                                              device_latitude=${req.body.device_latitude}, 
+                                              device_longitude=${req.body.device_longitude} 
+                                              WHERE id=${req.body.id} AND
+                                              (SELECT purchased_customer_email FROM device 
+                                              WHERE id=${req.body.id}) = ${req.email};`;
+    
+        execQuery(assignCustomertoDeviceQuery)
+          .then((rows) => {
+            res
+              .status(200)
+              .json({ message: "Device Specifications updated successfully" });
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      return res.sendStatus(401).json({ error: "Unauthorized" });
     }
+    
   });
   
   // delete device - [Done]
@@ -171,24 +297,29 @@ router.get("/", (req, res, next) => {
   // request format
   // {
   //   "deviceId": 123,
-  //   "adminId": 456,
   //   "password": "admin_password"
   // }
 
-  router.delete("/", (req, res, next) => {
-    try {
-      const deleteDeviceQuery = `DELETE FROM device WHERE id=${req.body.deviceId} AND ${req.body.password} = (SELECT passphrase FROM admin
-        WHERE admin.id=${req.body.adminId})`;
-      execQuery(deleteDeviceQuery)
-        .then((rows) => {
-          res.status(200).json({ message: "Admin Account Deleted Successfully" });
-        })
-        .catch((err) => {
-          next(err);
-        });
-    } catch (err) {
-      next(err);
+  router.delete("/", authenticateToken, (req, res, next) => {
+    if(req.user_type == "admin"){
+      try {
+        const deleteDeviceQuery = `DELETE FROM device WHERE id=${req.body.deviceId} 
+          AND (SELECT passphrase FROM admin
+          WHERE admin.id=${req.user_id}) = ${req.body.password}`;
+        execQuery(deleteDeviceQuery)
+          .then((rows) => {
+            res.status(200).json({ message: "Device Deleted Successfully" });
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      return res.sendStatus(401).json({ error: "Unauthorized" });
     }
+    
   });
   
 
