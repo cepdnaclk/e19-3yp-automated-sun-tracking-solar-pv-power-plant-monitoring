16086679b73ee88cd4cc25b17b9d7356ebc4a90b
Kavishkagaya <kavishkarambukwella@gmail.com>
2024-01-09 19:41:04 +0530

    added cors and json

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@
 
 // get libraries
 const express = require("express");
+const cors = require("cors");
 
 //initialize app
 const app = express();
 const port = 8081;
+
+app.use(cors());
+
+app.use(express.json()); // for parsing application/json
 
 //middleware setup
 
@@ -17,14 +22,14 @@ const port = 8081;
 // })
 
 //user authentication
-app.use("/login", require("./src/routes/login")); //edit this accordingly
+app.use("/api/login", require("./src/routes/login")); //edit this accordingly
 
 //jwt authentication
 //const { authenticateToken } = require("./src/middleware/auth");
 //app.use(authenticateToken)
 
 //main router
-app.use(require("./src/mainRouter"));
+app.use("/api", require("./src/mainRouter"));
 
 //database connection and query setup
 
@@ -34,8 +39,7 @@ app.use(require("./src/middleware/errorLogger"));
 // error response handling
 app.use(require("./src/middleware/errorHandler"));
 
-
 //start server
 app.listen(port, () => {
-    console.log(`The app listening on port ${port}`)
+  console.log(`The app listening on port ${port}`);
 });
