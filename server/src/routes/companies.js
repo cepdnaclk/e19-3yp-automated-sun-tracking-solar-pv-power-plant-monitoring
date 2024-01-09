06542598c73a211e67c397556c94ea07dc905f6a
Dinuka <e19029@eng.pdn.ac.lk>
2024-01-09 10:10:50 +0530

    company APIs done

--- server/src/routes/companies.js
+++ server/src/routes/companies.js
@@ -6,7 +6,9 @@ const router = express.Router();
 const {
   requestBodyToFieldsAndValues,
   objectKeysSnakeToCamel,
-} = require("../utils/parse");
+  } = require("../utils/parse");
+
+  const bcrypt = require("bcrypt");
 
 // to execute and get the output of the queries easily
 const { execQuery } = require("../database/database");
@@ -14,15 +16,13 @@ const { authenticateToken } = require("../middleware/auth");
 
 
 // get companies - if request has an id, get the specific user, if not get all users 
-// view all - only accessble for admins
-// view one profile - only for the respctive company
+// only accessble for admins
 // [Done]
 //change req.query to req.body if necessary
-router.get("/", authenticateToken, (req, res, next) => {
-    //only for the respective company
-    if(req.user_type == "company"){
+router.get("/view", authenticateToken, (req, res, next) => {
+    if(req.user_type == "admin"){
       if (req.query.id) {
-        execQuery(`SELECT id, username, email, passphrase, contact_number, user_address FROM user WHERE id=${req.query.id} AND username=${req.username}`)
+        execQuery(`SELECT id, username, email, contact_number, user_address FROM user WHERE id=${req.query.id} AND user_type="company"`)
           .then((rows) => {
             data = objectKeysSnakeToCamel(rows[0]);
             res.status(200).json(data);
@@ -31,8 +31,7 @@ router.get("/", authenticateToken, (req, res, next) => {
             next(err);
           });
       } else {
-        //for admins
-        execQuery(`SELECT id, username, email, passphrase, contact_number, user_address FROM user WHERE user_type="company"`)
+        execQuery(`SELECT id, username, email, contact_number, user_address FROM user WHERE user_type="company"`)
           .then((rows) => {
             data = rows[0].map((row) => objectKeysSnakeToCamel(row));
             res.status(200).json(data);
@@ -46,6 +45,24 @@ router.get("/", authenticateToken, (req, res, next) => {
     }
     
   });
+
+  // get own profile - only for the respective company - [Done]
+  router.get("/myprofile", authenticateToken, (req, res, next) => {
+    if(req.user_type == "company"){
+       //view own profile
+        execQuery(`SELECT id, username, email, contact_number, user_address
+                  FROM user
+                  WHERE id = ${req.user_id}}`)
+          .then((rows) => {
+            data = objectKeysSnakeToCamel(rows[0]);
+            res.status(200).json(data);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      }}
+  );
+
   
   //add new company - only accessible for admins
   //[Done]
@@ -58,11 +75,10 @@ router.get("/", authenticateToken, (req, res, next) => {
     //   contact_number: "1234567890",
     //   user_address: "123 Main Street, Cityville"
     // }
-  router.post("/",authenticateToken, (req, res, next) => {
+  router.post("/new",authenticateToken, async(req, res, next) => {
     if(req.user_type == "admin"){
       try {
-        // const [fields, values] = requestBodyToFieldsAndValues(req.body);
-        // delete req.body["id"];
+        req.body.passphrase = await bcrypt.hash(req.body.passphrase, 10);
         const [fields, values] = [Object.keys(req.body), Object.values(req.body)];
         const createCompanyQuery = `INSERT INTO user (${fields.toString()}) VALUES (${values.toString()})`;
     
@@ -85,19 +101,17 @@ router.get("/", authenticateToken, (req, res, next) => {
   //update company (my profile) details - only accessble companies to update their own details
   // [Done]
 // {
-//   id : 123456
 //   username: "john_doe",
 //   user_type: "admin",
 //   email: "john.doe@example.com",
-//   passphrase: "securepassword",
 //   contact_number: "1234567890",
 //   user_address: "123 Main Street, Cityville"
 // }
   
-  router.put("/", authenticateToken, (req, res, next) => {
+  router.put("/update", authenticateToken, (req, res, next) => {
     if(req.user_type == "company"){
       try {
-        const id = req.body["id"];
+        const id = req.user_id;
         delete req.body["id"]; //id used in the UPDATE query, not needed in the update string
         const [fields, values] = [Object.keys(req.body), Object.values(req.body)];
         // Combine the two arrays into a single array.
@@ -129,9 +143,41 @@ router.get("/", authenticateToken, (req, res, next) => {
       return res.sendStatus(401).json({ error: "Unauthorized" });
     }
   });
+
+  //create a put request to change password [Done]
+// request format
+// {
+//   old_password: "securepassword",
+//   new_password: "newsecurepassword"
+// }  
+router.put("/changePassword", authenticateToken, async (req, res, next) => {
+  if(req.user_type == "company"){
+    try {
+      const id = req.user_id;
+      const old_password = req.body["old_password"];
+      const new_password = req.body["new_password"];
+      const getPassphraseQuery = `SELECT passphrase FROM user WHERE id=${id}`;
+      const rows = await execQuery(getPassphraseQuery);
+      const passphrase = rows[0][0]["passphrase"];
+      const isMatch = await bcrypt.compare(old_password, passphrase);
+
+      if (isMatch) {
+        const hashedPassword = await bcrypt.hash(new_password, 10);
+        const updatePassphraseQuery = `UPDATE user SET passphrase='${hashedPassword}' WHERE id=${id}`;
+        await execQuery(updatePassphraseQuery);
+        res.status(200).json({ message: "Password changed successfully" });
+      } else {
+        res.status(401).json({ error: "Invalid Password" });
+      }
+    } catch (err) {
+      next(err);
+    }
+} else {
+  return res.sendStatus(401).json({ error: "Unauthorized" });
+}
+});
   
-  // delete companies
-  // change to req.body.id if necessary
+  // delete companies - [Done]
   // only accessible for admins
 
   // request format
@@ -140,21 +186,24 @@ router.get("/", authenticateToken, (req, res, next) => {
   //   "password": "admin_password"
   // }
 
-  router.delete("/", (req, res, next) => {
-    if(req.user_type == "admin"){
+  router.delete("/deleteCompany", authenticateToken, async (req, res, next) => {
+    if(req.user_type == "customer"){
       try {
-        const deleteCompanyQuery = `DELETE FROM user WHERE id=${req.body.companyId} 
-                                    AND ${req.body.password} = 
-                                    (SELECT passphrase FROM user
-                                    WHERE username=${req.username}) `;
+        const id = req.user_id;
+        const password = req.body["password"];
+        const getPassphraseQuery = `SELECT passphrase FROM user WHERE id=${id}`;
+        const rows = await execQuery(getPassphraseQuery);
+        const passphrase = rows[0][0]["passphrase"];
+        const isMatch = await bcrypt.compare(password, passphrase);
   
-        execQuery(deleteCompanyQuery)
-          .then((rows) => {
-            res.status(200).json({ message: "Admin Account Deleted Successfully" });
-          })
-          .catch((err) => {
-            next(err);
-          });
+        if (isMatch) {
+          const deleteCompanyQuery = `DELETE FROM user WHERE id=${req.body.companyId} 
+          AND user_type="company"`;
+          await execQuery(deleteCompanyQuery);
+          res.status(200).json({ message: "Customer Account Deleted Successfully" });
+        } else {
+          res.status(401).json({ error: "Invalid Password" });
+        }
       } catch (err) {
         next(err);
       }
