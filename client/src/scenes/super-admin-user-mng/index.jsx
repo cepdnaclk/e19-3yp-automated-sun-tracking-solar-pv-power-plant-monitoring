2d83d0fb0f178cca5b8c6209addba6ca9b28104a
inesha21 <e19090@eng.pdn.ac.lk>
2024-01-09 11:14:40 +0530

    updated super admin user mng and profile

--- client/src/scenes/super-admin-user-mng/index.jsx
+++ client/src/scenes/super-admin-user-mng/index.jsx
@@ -1,162 +1,126 @@
 import React from 'react';
-import { useTheme } from '@mui/system';
+import Header from '../../components/Header';
+import { Box, Button, IconButton, Typography, useTheme } from '@mui/material';
 import { tokens } from '../../theme';
-import { Typography, Checkbox, FormControlLabel, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
+
+import { DataGrid } from '@mui/x-data-grid';
+
+// super admin user data
+import { superAdminUserData } from '../../data/superAdminUserData';
 
 const SuperAdminUserMng = () => {
 	const theme = useTheme();
-  	const colors = tokens(theme.palette.mode);
+	const colors = tokens(theme.palette.mode);
 
-	const headingStyle = {
-	  fontSize: '20px',
-	  fontWeight: 'bold',
-	  margin: '10px 0 10px 50px',
-	  color: '#FFAC09',
-	  marginLeft: '20px',
+	// columns of the data grid
+	const columns = [
+        { field: 'id', headerName: 'ID' },
+		{ field: 'companyId', headerName: 'Company ID' },
+		{
+			field: 'companyName',
+			headerName: 'Company Name',
+			flex: 1,
+			cellClassName: 'name-column--cell',
+		},
+		{
+			field: 'devices',
+			headerName: 'Devices',
+			flex: 1,
+		},
+		{ field: 'registered', headerName: 'Registered', flex: 1 },
+		{ field: 'unregistered', headerName: 'Unregistered', flex: 1 },
+		{
+			field: 'actions',
+			headerName: 'Actions',
+			flex: 1,
+			renderCell: (params) => (
+				<>
+					<Button
+						variant="outlined"
+						size="small"
+						onClick={() => handleEdit(params.id)} // Use params.id instead of params.row.deviceId
+						sx={{
+							ml: 2,
+							color: colors.yellowAccent[400],
+							borderColor: colors.yellowAccent[400],
+						}}
+					>
+						Edit
+					</Button>
+					<Button
+						variant="outlined"
+						color="error"
+						size="small"
+						onClick={() => handleDelete(params.id)} // Use params.id instead of params.row.deviceId
+						sx={{ ml: 1 }}
+					>
+						Delete
+					</Button>
+				</>
+			),
+		},
+	];
+
+	const handleEdit = (companyId) => {
+		// Handle edit logic here
+		console.log(`Editing device with ID: ${companyId}`);
 	};
 
-    const filterSectionStyle = {
-        display: 'flex',
-        marginLeft: '20px',
-        alignItems: 'center', // Center align the checkboxes with the labels
-    };
+	const handleDelete = (companyId) => {
+		// Handle delete logic here
+		console.log(`Deleting device with ID: ${companyId}`);
+	};
 
-    const tableStyle = {
-        marginLeft: '20px',
-        marginTop: '20px',
-        width: '80%',
-    };
+	return (
+		<Box m="20px" width="90%">
+			{/* Header */}
+			<Box
+				display="flex"
+				justifyContent="space-between"
+				alignItems="center"
+			>
+				<Header
+					title="USER MANAGEMENT"
+					subtitle="Manage Users and Device Overview"
+				/>
+			</Box>
 
-    const columnStyle = {
-		
-        maxWidth: '200px', // Adjust the maximum width as needed
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-        whiteSpace: 'nowrap',
-    };
-
-	const editStyle = {
-        marginRight: '10px',
-		fontSize: '15px',
-		color: '#FFAC09',
-    };
-
-	const removeStyle = {
-        marginLeft: '10px',
-		fontSize: '15px',
-		color: '#FF610A',
-    };
-
-    return (
-        <div>
-			<Typography variant="h2" style={headingStyle}>
-				User Management
-			</Typography>
-
-            <div style={filterSectionStyle}>
-                <span style={{ marginRight: '10px' }}>Filter by:</span>
-                <div style={{ display: 'flex', alignItems: 'center', marginRight: '20px' }}>
-                    <FormControlLabel
-                        label="Company"
-                        control={<Checkbox />}
-                        labelPlacement="start"
-                    />
-                </div>
-                <div style={{ display: 'flex', alignItems: 'center', marginRight: '20px' }}>
-                    <FormControlLabel
-                        label="Devices"
-                        control={<Checkbox />}
-                        labelPlacement="start"
-                    />
-                </div>
-                <div style={{ display: 'flex', alignItems: 'center', marginRight: '20px' }}>
-                    <FormControlLabel
-                        label="Registered"
-                        control={<Checkbox />}
-                        labelPlacement="start"
-                    />
-                </div>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <FormControlLabel
-                        label="Unregistered"
-                        control={<Checkbox />}
-                        labelPlacement="start"
-                    />
-                </div>
-                <div style={{ marginLeft: '30px', color: '#FFAC09' }}>Save</div>
-            </div>
-
-			<div>
-				<Button href="/super-admin-add-user" color="secondary" size="small" style={{ background: '#FFAC09', color: 'black', marginLeft: '928px', fontWeight: 'bold' }}>
-					<AddIcon style={{ marginRight: '4px' }} /> Add Company
-				</Button>
-			</div>
-
-            <div style={tableStyle}>
-                <TableContainer component={Paper}>
-                    <Table style={{ background: `${colors.primary[400]}` }}>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell></TableCell>
-                                <TableCell>Company</TableCell>
-                                <TableCell>Devices</TableCell>
-                                <TableCell>Registered</TableCell>
-                                <TableCell>Unregistered</TableCell>
-                                <TableCell>Actions</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell><Checkbox /></TableCell>
-                                <TableCell style={columnStyle}>Company name</TableCell>
-                                <TableCell style={columnStyle}>50</TableCell>
-                                <TableCell style={columnStyle}>40</TableCell>
-                                <TableCell style={columnStyle}>10</TableCell>
-                                <TableCell style={columnStyle}>
-                                    <a style={editStyle} href="#edit">Edit</a>
-                                    <a style={removeStyle} href="#remove">Remove</a>
-                                </TableCell>
-                            </TableRow>
-							<TableRow>
-                                <TableCell><Checkbox /></TableCell>
-                                <TableCell style={columnStyle}>Company name</TableCell>
-                                <TableCell style={columnStyle}>50</TableCell>
-                                <TableCell style={columnStyle}>40</TableCell>
-                                <TableCell style={columnStyle}>10</TableCell>
-                                <TableCell >
-                                    <a style={editStyle} href="#edit">Edit</a>
-                                    <a style={removeStyle} href="#remove">Remove</a>
-                                </TableCell>
-                            </TableRow>
-							<TableRow>
-                                <TableCell><Checkbox /></TableCell>
-                                <TableCell style={columnStyle}>Company name</TableCell>
-                                <TableCell style={columnStyle}>50</TableCell>
-                                <TableCell style={columnStyle}>40</TableCell>
-                                <TableCell style={columnStyle}>10</TableCell>
-                                <TableCell style={columnStyle}>
-                                    <a style={editStyle} href="#edit">Edit</a>
-                                    <a style={removeStyle} href="#remove">Remove</a>
-                                </TableCell>
-                            </TableRow>
-							<TableRow>
-                                <TableCell><Checkbox /></TableCell>
-                                <TableCell style={columnStyle}>Company name</TableCell>
-                                <TableCell style={columnStyle}>50</TableCell>
-                                <TableCell style={columnStyle}>40</TableCell>
-                                <TableCell style={columnStyle}>10</TableCell>
-                                <TableCell style={columnStyle}>
-                                    <a style={editStyle} href="#edit">Edit</a>
-                                    <a style={removeStyle} href="#remove">Remove</a>
-                                </TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
-        </div>
-    );
+			<Box
+				height="75vh"
+				sx={{
+					'& .MuiDataGrid-root': {
+						border: 'none',
+					},
+					'& .MuiDataGrid-cell': {
+						borderBottom: 'none',
+					},
+					'& .name-column--cell': {
+						color: colors.yellowAccent[300],
+					},
+					'& .MuiDataGrid-columnHeaders': {
+						backgroundColor: colors.blueAccent[700],
+						borderBottom: 'none',
+					},
+					'& .MuiDataGrid-virtualScroller': {
+						backgroundColor: colors.primary[400],
+					},
+					'& .MuiDataGrid-footerContainer': {
+						borderTop: 'none',
+						backgroundColor: colors.blueAccent[700],
+					},
+					'& .MuiCheckbox-root': {
+						color: `${colors.yellowAccent[200]} !important`,
+					},
+				}}
+			>
+				<DataGrid
+					checkboxSelection
+					rows={superAdminUserData}
+					columns={columns}
+				/>
+			</Box>
+		</Box>
+	);
 };
 
 export default SuperAdminUserMng;
