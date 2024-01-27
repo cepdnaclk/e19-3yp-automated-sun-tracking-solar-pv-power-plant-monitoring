6033a2667095cb6fc81c829b240070ac85877d9b
dhanushka <e18059@eng.pdn.ac.lk>
2024-01-27 22:12:08 +0530

    Updated admin device mng

--- client/src/scenes/super-admin-device-mng/index.jsx
+++ client/src/scenes/super-admin-device-mng/index.jsx
@@ -1,155 +1,241 @@
-import { Box, Button, useTheme } from "@mui/material";
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import Header from "../../components/Header";
-import { tokens } from "../../theme";
-
-import { DataGrid } from "@mui/x-data-grid";
+import { Box, Button, useTheme } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import {
+	GridRowModes,
+	DataGrid,
+	GridActionsCellItem,
+	GridRowEditStopReasons,
+} from '@mui/x-data-grid';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
+import { tokens } from '../../theme';
 
 const SuperAdminDeviceMng = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const [superAdminDeviceData, setSuperAdminDeviceData] = useState([]);
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+	const [superAdminDeviceData, setSuperAdminDeviceData] = useState([]);
+	const [rows, setRows] = useState([]);
+	const [rowModesModel, setRowModesModel] = useState({});
 
-  useEffect(() => {
-    axios
-      .get("/devices/view")
-      .then((res) => {
-        setSuperAdminDeviceData(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+	const handleRowEditStop = (params, event) => {
+		if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+			event.defaultMuiPrevented = true;
+		}
+	};
 
-  // columns of the data grid
-  const columns = [
-    { field: "id", headerName: "ID" },
-    { field: "modelName", 
-      headerName: "Model Name", 
-      flex: 1 },
-    {
-      field: "modelNumber",
-      headerName: "Model Number",
-      flex: 1, },
-    { field: "assignedCompanyId", 
-      headerName: "Company ID", 
-      flex: 1, },
-    {
-      field: "assignedCompanyName",
-      headerName: "Company Name",
-      flex: 1,
-      cellClassName: "name-column--cell", },
-    {
-      field: "actions",
-      headerName: "Actions",
-      flex: 1,
-      renderCell: (params) => (
-        <>
-          <Button
-            variant="outlined"
-            size="small"
-            onClick={() => handleEdit(params.id)} // Use params.id instead of params.row.deviceId
-            sx={{
-              ml: 2,
-              color: colors.yellowAccent[400],
-              borderColor: colors.yellowAccent[400],
-            }}
-          >
-            Edit
-          </Button>
-          <Button
-            variant="outlined"
-            color="error"
-            size="small"
-            onClick={() => handleDelete(params.id)} // Use params.id instead of params.row.deviceId
-            sx={{ ml: 1 }}
-          >
-            Delete
-          </Button>
-        </>
-      ),
-    },
-  ];
+	const handleEditClick = (id) => () => {
+		setRowModesModel({
+			...rowModesModel,
+			[id]: { mode: GridRowModes.Edit },
+		});
+	};
 
-  const handleEdit = (companyId) => {
-    // Handle edit logic here
-    console.log(`Editing device with ID: ${deviceId}`);
-  };
+	const handleSaveClick = (id) => () => {
+		setRowModesModel({
+			...rowModesModel,
+			[id]: { mode: GridRowModes.View },
+		});
+	};
 
-  const handleDelete = (companyId) => {
-    // Handle delete logic here
-    console.log(`Deleting device with ID: ${deviceId}`);
-  };
+	const handleDeleteClick = (id) => () => {
+		setRows(rows.filter((row) => row.id !== id));
+	};
 
-  return (
-    <Box m="10px 20px 20px 20px" width="90%">
-      {/* Header */}
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header
-          title="DEVICE MANAGEMENT"
-          subtitle="Manage Devices and Device Overview"
-        />
-      </Box>
+	const handleCancelClick = (id) => () => {
+		setRowModesModel({
+			...rowModesModel,
+			[id]: { mode: GridRowModes.View, ignoreModifications: true },
+		});
 
-      <Box>
-        <Link
-          to="/super-admin-add-device"
-          style={{ textDecoration: "none", margin: "1rem 0", display: "flex" }}
-        >
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            style={{
-              background: "#FFAC09",
-              color: "black",
-              fontWeight: "bold",
-              marginLeft: "auto",
-            }}
-          >
-            Add New Device
-          </Button>
-        </Link>
-      </Box>
+		const editedRow = rows.find((row) => row.id === id);
+		if (editedRow.isNew) {
+			setRows(rows.filter((row) => row.id !== id));
+		}
+	};
 
-      <Box
-        height="75vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .name-column--cell": {
-            color: colors.yellowAccent[300],
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: colors.blueAccent[700],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: colors.primary[400],
-          },
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-            backgroundColor: colors.blueAccent[700],
-          },
-          "& .MuiCheckbox-root": {
-            color: `${colors.yellowAccent[200]} !important`,
-          },
-        }}
-      >
-        <DataGrid
-          checkboxSelection
-          rows={superAdminDeviceData}
-          columns={columns}
-        />
-      </Box>
-    </Box>
-  );
+	const processRowUpdate = (newRow) => {
+		const updatedRow = { ...newRow, isNew: false };
+		setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+		return updatedRow;
+	};
+
+	const handleRowModesModelChange = (newRowModesModel) => {
+		setRowModesModel(newRowModesModel);
+	};
+
+	useEffect(() => {
+		axios
+			.get('/devices/view')
+			.then((res) => {
+				setSuperAdminDeviceData(res.data);
+				setRows(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	const columns = [
+		{ field: 'id', headerName: 'ID' },
+		{
+			field: 'modelName',
+			headerName: 'Model Name',
+			flex: 1,
+			editable: true,
+		},
+		{
+			field: 'modelNumber',
+			headerName: 'Model Number',
+			flex: 1,
+			editable: true,
+		},
+		{
+			field: 'assignedCompanyId',
+			headerName: 'Company ID',
+			flex: 1,
+			editable: true,
+		},
+		{
+			field: 'assignedCompanyName',
+			headerName: 'Company Name',
+			flex: 1,
+			cellClassName: 'name-column--cell',
+			editable: true,
+		},
+		{
+			field: 'actions',
+			headerName: 'Actions',
+			flex: 1,
+			type: 'actions',
+			cellClassName: 'actions',
+			getActions: ({ id }) => {
+				const isInEditMode =
+					rowModesModel[id]?.mode === GridRowModes.Edit;
+
+				if (isInEditMode) {
+					return [
+						<GridActionsCellItem
+							icon={<SaveIcon />}
+							label="Save"
+							sx={{
+								color: 'primary.main',
+							}}
+							onClick={handleSaveClick(id)}
+						/>,
+						<GridActionsCellItem
+							icon={<CancelIcon />}
+							label="Cancel"
+							className="textPrimary"
+							onClick={handleCancelClick(id)}
+							color="inherit"
+						/>,
+					];
+				}
+
+				return [
+					<GridActionsCellItem
+						icon={<EditIcon />}
+						label="Edit"
+						className="textPrimary"
+						onClick={handleEditClick(id)}
+						color="inherit"
+					/>,
+					<GridActionsCellItem
+						icon={<DeleteIcon />}
+						label="Delete"
+						onClick={handleDeleteClick(id)}
+						color="inherit"
+					/>,
+				];
+			},
+		},
+	];
+
+	return (
+		<Box m="10px 20px 20px 20px" width="90%">
+			<Box
+				display="flex"
+				justifyContent="space-between"
+				alignItems="center"
+			>
+				<Header
+					title="DEVICE MANAGEMENT"
+					subtitle="Manage Devices and Device Overview"
+				/>
+			</Box>
+
+			<Box>
+				<Link
+					to="/super-admin-add-device"
+					style={{
+						textDecoration: 'none',
+						margin: '1rem 0',
+						display: 'flex',
+					}}
+				>
+					<Button
+						type="submit"
+						variant="contained"
+						color="primary"
+						style={{
+							background: '#FFAC09',
+							color: 'black',
+							fontWeight: 'bold',
+							marginLeft: 'auto',
+						}}
+					>
+						Add New Device
+					</Button>
+				</Link>
+			</Box>
+
+			<Box
+				height="75vh"
+				sx={{
+					'& .MuiDataGrid-root': {
+						border: 'none',
+					},
+					'& .MuiDataGrid-cell': {
+						borderBottom: 'none',
+					},
+					'& .name-column--cell': {
+						color: colors.yellowAccent[300],
+					},
+					'& .MuiDataGrid-columnHeaders': {
+						backgroundColor: colors.blueAccent[700],
+						borderBottom: 'none',
+					},
+					'& .MuiDataGrid-virtualScroller': {
+						backgroundColor: colors.primary[400],
+					},
+					'& .MuiDataGrid-footerContainer': {
+						borderTop: 'none',
+						backgroundColor: colors.blueAccent[700],
+					},
+					'& .MuiCheckbox-root': {
+						color: `${colors.yellowAccent[200]} !important`,
+					},
+				}}
+			>
+				<DataGrid
+					checkboxSelection
+					rows={superAdminDeviceData}
+					columns={columns}
+					editMode="row"
+					rowModesModel={rowModesModel}
+					onRowModesModelChange={handleRowModesModelChange}
+					onRowEditStop={handleRowEditStop}
+					processRowUpdate={processRowUpdate}
+				/>
+			</Box>
+		</Box>
+	);
 };
 
 export default SuperAdminDeviceMng;
