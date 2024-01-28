ae9fe60f31fb403c7ca5d76ae394aed08ba56e82
dhanushka <e18059@eng.pdn.ac.lk>
2024-01-28 21:39:15 +0530

    updated admin-device-mng

--- client/src/scenes/admin-device-mng/index.jsx
+++ client/src/scenes/admin-device-mng/index.jsx
@@ -1,88 +1,208 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import {
+	GridRowModes,
+	DataGrid,
+	GridActionsCellItem,
+	GridRowEditStopReasons,
+} from '@mui/x-data-grid';
 import Header from '../../components/Header';
 import { Link } from 'react-router-dom';
-import axios from "axios";
+import axios from 'axios';
 import { Box, Button, useTheme } from '@mui/material';
 import { tokens } from '../../theme';
-
-import { DataGrid } from '@mui/x-data-grid';
 
 const AdminDeviceMng = () => {
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
 	const [adminDeviceData, setAdminDeviceData] = useState([]);
+	const [rows, setRows] = useState([]);
+	const [rowModesModel, setRowModesModel] = useState({});
 
 	useEffect(() => {
 		axios
-		  .get("/devices/")
-		  .then((res) => {
-			setAdminDeviceData(res.data);
-		  })
-		  .catch((err) => {
-			console.log(err);
-		  });
-	  }, []);
+			.get('/devices/')
+			.then((res) => {
+				setAdminDeviceData(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	// data-grid functions
+	const handleRowEditStop = (params, event) => {
+		if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+			event.defaultMuiPrevented = true;
+		}
+	};
+
+	const handleEditClick = (id) => () => {
+		setRowModesModel({
+			...rowModesModel,
+			[id]: { mode: GridRowModes.Edit },
+		});
+	};
+
+	const handleSaveClick = (id) => () => {
+		setRowModesModel({
+			...rowModesModel,
+			[id]: { mode: GridRowModes.View },
+		});
+	};
+
+	const handleDeleteClick = (id) => () => {
+		// Get the index of the row to be deleted
+		const rowIndex = adminDeviceData.findIndex((row) => row.id === id);
+
+		// Remove the row from the data
+		const updatedData = [...adminDeviceData];
+		updatedData.splice(rowIndex, 1);
+
+		setAdminDeviceData(updatedData);
+
+		setRowModesModel({
+			...rowModesModel,
+			[id]: { mode: GridRowModes.View, ignoreModifications: true },
+		});
+
+		// Uncomment the following lines if you want to make a delete request to the API
+		// const password = prompt('Enter your password for verification:', '');
+		// axios
+		// 	.delete('/companies/', {
+		// 		data: {
+		// 			companyId: id,
+		// 			password: password,
+		// 		},
+		// 	})
+		// 	.then((res) => {
+		// 		// Handle the response if needed
+		// 		console.log(res.data);
+		// 	})
+		// 	.catch((err) => {
+		// 		console.error(err);
+		// 		// Handle the error
+		// 	});
+	};
+
+	const handleCancelClick = (id) => () => {
+		const editedRow = rows.find((row) => row.id === id);
+
+		if (editedRow) {
+			if (editedRow.isNew) {
+				setRows((prevRows) => prevRows.filter((row) => row.id !== id));
+			}
+
+			setRowModesModel({
+				...rowModesModel,
+				[id]: {
+					mode: GridRowModes.View,
+					ignoreModifications: true,
+				},
+			});
+		}
+	};
+
+	const processRowUpdate = (newRow) => {
+		const updatedRow = { ...newRow, isNew: false };
+		setRows((prevRows) =>
+			prevRows.map((row) => (row.id === newRow.id ? updatedRow : row))
+		);
+		return updatedRow;
+	};
+
+	const handleRowModesModelChange = (newRowModesModel) => {
+		setRowModesModel(newRowModesModel);
+	};
 
 	// columns of the data grid
 	const columns = [
-		{ field: 'id', headerName: 'ID', },
+		{ field: 'id', headerName: 'ID' },
 		{
 			field: 'modelName',
 			headerName: 'Model Name',
 			flex: 1,
 			cellClassName: 'name-column--cell',
 			width: 150,
+			editable: true,
 		},
-		{ field: 'modelNumber', headerName: 'Model Number', width: 150, },
+		{
+			field: 'modelNumber',
+			headerName: 'Model Number',
+			width: 150,
+			editable: true,
+		},
 		{
 			field: 'deviceDescription',
 			headerName: 'Description',
 			flex: 1,
 			cellClassName: 'name-column--cell',
 			width: 200,
+			editable: true,
 		},
-		{ field: 'purchasedCustomerEmail', headerName: 'Customer Email', flex: 1 },
+		{
+			field: 'purchasedCustomerEmail',
+			headerName: 'Customer Email',
+			flex: 1,
+			editable: true,
+		},
 		{
 			field: 'actions',
 			headerName: 'Actions',
 			flex: 1,
-			renderCell: (params) => (
-				<>
-					<Button
-						variant="outlined"
-						size="small"
-						onClick={() => handleEdit(params.id)} // Use params.id instead of params.row.deviceId
+			type: 'actions',
+			cellClassName: 'actions',
+			getActions: ({ id }) => {
+				const isInEditMode =
+					rowModesModel[id]?.mode === GridRowModes.Edit;
+
+				if (isInEditMode) {
+					return [
+						<GridActionsCellItem
+							icon={<SaveIcon />}
+							label="Save"
+							sx={{
+								color: colors.yellowAccent[400],
+							}}
+							onClick={handleSaveClick(id)}
+						/>,
+						<GridActionsCellItem
+							icon={<CancelIcon />}
+							label="Cancel"
+							className="textPrimary"
+							onClick={handleCancelClick(id)}
+							sx={{
+								color: colors.redAccent[500],
+							}}
+						/>,
+					];
+				}
+
+				return [
+					<GridActionsCellItem
+						icon={<EditIcon />}
+						label="Edit"
+						className="textPrimary"
+						onClick={handleEditClick(id)}
 						sx={{
-							ml: 2,
 							color: colors.yellowAccent[400],
-							borderColor: colors.yellowAccent[400],
 						}}
-					>
-						Edit
-					</Button>
-					<Button
-						variant="outlined"
-						color="error"
-						size="small"
-						onClick={() => handleDelete(params.id)} // Use params.id instead of params.row.deviceId
-						sx={{ ml: 1 }}
-					>
-						Delete
-					</Button>
-				</>
-			),
+					/>,
+					<GridActionsCellItem
+						icon={<DeleteIcon />}
+						label="Delete"
+						onClick={handleDeleteClick(id)}
+						sx={{
+							color: colors.redAccent[500],
+						}}
+					/>,
+				];
+			},
 		},
 	];
-
-	const handleEdit = (deviceId) => {
-		// Handle edit logic here
-		console.log(`Editing device with ID: ${deviceId}`);
-	};
-
-	const handleDelete = (deviceId) => {
-		// Handle delete logic here
-		console.log(`Deleting device with ID: ${deviceId}`);
-	};
 
 	return (
 		<Box m="20px" width="90%">
@@ -99,25 +219,29 @@ const AdminDeviceMng = () => {
 			</Box>
 
 			<Box>
-        <Link
-          to="/admin-add-device"
-          style={{ textDecoration: "none", margin: "1rem 0", display: "flex" }}
-        >
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            style={{
-              background: "#FFAC09",
-              color: "black",
-              fontWeight: "bold",
-              marginLeft: "auto",
-            }}
-          >
-            Add New Device
-          </Button>
-        </Link>
-      </Box>
+				<Link
+					to="/admin-add-device"
+					style={{
+						textDecoration: 'none',
+						margin: '1rem 0',
+						display: 'flex',
+					}}
+				>
+					<Button
+						type="submit"
+						variant="contained"
+						color="primary"
+						style={{
+							background: '#FFAC09',
+							color: 'black',
+							fontWeight: 'bold',
+							marginLeft: 'auto',
+						}}
+					>
+						Add New Device
+					</Button>
+				</Link>
+			</Box>
 
 			<Box
 				height="75vh"
@@ -151,6 +275,13 @@ const AdminDeviceMng = () => {
 					checkboxSelection
 					rows={adminDeviceData}
 					columns={columns}
+					editMode="row"
+					rowModesModel={rowModesModel}
+					onRowModesModelChange={handleRowModesModelChange}
+					onRowEditStop={handleRowEditStop}
+					onSelectionModelChange={(newSelection) => {
+						// Handle selection changes if needed
+					}}
 				/>
 			</Box>
 		</Box>
